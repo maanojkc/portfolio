7a66bfe94f6d1c0db1b4dd10b07f9b0286f664a7
Manoj <manojkc8431@gmail.com>
2026-04-05 18:34:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,21 @@
 const terminal = document.getElementById("terminal");
 const main = document.getElementById("main");
 
-const lines = [
-  "booting system...",
-  "loading portfolio...",
-  "access granted ✔",
-  "welcome Manoj",
-];
+const text = "initializing portfolio...";
 
 let i = 0;
 
-function typeLine() {
-  if (i < lines.length) {
-    let line = document.createElement("p");
-    terminal.appendChild(line);
-
-    let text = lines[i];
-    let j = 0;
-
-    let interval = setInterval(() => {
-      line.textContent += text[j];
-      j++;
-
-      if (j === text.length) {
-        clearInterval(interval);
-        i++;
-        setTimeout(typeLine, 500);
-      }
-    }, 50);
+function type() {
+  if (i < text.length) {
+    terminal.textContent += text[i];
+    i++;
+    setTimeout(type, 40);
   } else {
     setTimeout(() => {
       terminal.style.display = "none";
       main.classList.remove("hidden");
-    }, 1000);
+    }, 600);
   }
 }
 
-typeLine();
+type();
